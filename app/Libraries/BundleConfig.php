772ce191a98b8ff2df109bfa9218ac2dd868f81c
Rizal Zaelani <zaelanirizal.rz@gmail.com>
2021-10-14 07:15:11 +0700

    Schedule Page beta

--- app/Libraries/BundleConfig.php
+++ app/Libraries/BundleConfig.php
@@ -23,6 +23,7 @@ class BundleConfig
 			base_url('/vendors/filepond/filepond-plugin-file-validate-type.js'),
 			base_url('/vendors/filepond/filepond.min.js'),
 			base_url('/vendors/filepond/filepond.jquery.js'),
+			base_url('/vendors/fullcalendar/core/js/main.js'),
 			base_url('/vendors/croppie/croppie.min.js'),
 			// base_url('/vendors/mapbox/mapbox-gl.js.map'),
 			base_url('/vendors/jquery-ui/jquery-ui.js'),
@@ -48,6 +49,7 @@ class BundleConfig
 			base_url('/vendors/select2/css/select2-coreui.min.css'),
 			base_url('/vendors/select2/css/select2.min.css'),
 			base_url('/vendors/filepond/filepond.css'),
+			base_url('/vendors/fullcalendar/css/main.css'),
 			base_url('/vendors/croppie/croppie.css'),
 			base_url('/vendors/jquery-ui/jquery-ui.css'),
 
